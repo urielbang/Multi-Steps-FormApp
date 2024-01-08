74cf3d3180f404e5b7e790762604a0aeb24f17cb
urielbang <uri32655@gmail.com>
2024-01-08 18:48:29 +0200

    form vaildation

--- src/components/YourInfo.jsx
+++ src/components/YourInfo.jsx
@@ -1,4 +1,29 @@
+import { Link } from "react-router-dom";
 function yourInfo() {
-  return <div>yourn info!</div>;
+  return (
+    <div>
+      <h1>Personal info</h1>
+      <span> Please provide your name, email address, and phone number.</span>
+      <form className="formInfo">
+        <label htmlFor="name">Name</label>
+        <input id="name" type="text" placeholder="e.g. Stephen King"></input>
+        <label htmlFor="emailInput">Email Address</label>
+        <input
+          id="emailInput"
+          type="email"
+          placeholder="e.g. stephenking@lorem.com"
+        ></input>
+        <label htmlFor="phoneInput">Phone Number</label>
+        <input
+          id="phoneInput"
+          type="number"
+          placeholder=" e.g. +1 234 567 890"
+        ></input>
+        <Link to={"/SelectPlan"}>
+          <button>Next Step</button>
+        </Link>
+      </form>
+    </div>
+  );
 }
 export default yourInfo;
